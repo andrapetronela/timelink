51c7aef401584defe1149438fd0d4d23e2e2670b
Andra <andra.popa1989@gmail.com>
2018-08-29 23:05:31 +0100

    updated subscribe section

--- c_script.js
+++ c_script.js
@@ -3,11 +3,11 @@ var description = document.getElementById('desc');
 var p = document.getElementById('p');
 
 description.addEventListener('mouseenter', function() {
-    about.style.backgroundColor = '#eee';
+    about.style.backgroundColor = '#F2F1EF';
     p.style.color = '#111';
 })
 
 description.addEventListener('mouseout', () => {
     about.style.backgroundColor = '#000';
-    p.style.color = '#fff';
+    p.style.color = '#F2F1EF';
 })
